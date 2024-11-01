2e066576a9a6cd8446e5cec5f42e112895c5e837
Aesgarth <getflair@gmail.com>
2024-11-01 21:01:25 +0000

    Update __init__.py

--- printer_notification/__init__.py
+++ printer_notification/__init__.py
@@ -1,4 +1,9 @@
-def setup(hass, config):
-    """Set up the printer notification component."""
-    hass.services.register_notification_service("printer_notification", PrinterNotificationService)
+async def async_setup_entry(hass, config_entry):
+    """Set up Printer Notification from a config entry."""
+    ip_address = config_entry.data.get("ip_address")
+    port = config_entry.data.get("port", 9100)
+
+    # Pass the config data to the PrinterNotificationService
+    hass.services.register_notification_service("printer_notification", PrinterNotificationService(ip_address, port))
+
     return True
